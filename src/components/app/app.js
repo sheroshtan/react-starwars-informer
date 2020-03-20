7c1d5bf5700cb43705dbf85f5a3843d55aad179c
sheroshtan <sheroshtan1@gmail.com>
2020-03-20 18:14:38 +0300

    component random-planet was stylized and connected to server. created state for the component and added data to render

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,16 +9,20 @@ const App = () => {
     return (
         <div>
             <Header />
-            <RandomPlanet />
 
-            <div className="row mb2">
-                <div className="col-md-6">
-                    <ItemList />
-                </div>
-                <div className="col-md-6">
-                    <PersonDetails />
+            <div className="container">
+                <RandomPlanet />
+                <div className="row">
+                    <div className="col-md-6">
+                        <ItemList />
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails />
+                    </div>
                 </div>
             </div>
         </div>
     )
 };
+
+export default App;
